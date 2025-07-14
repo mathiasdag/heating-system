e38421cb983d542b52faa7689d257893c3cd1886
mathiasdag <mathias@asso.gd>
2025-07-14 17:23:07 +0200

    feat(Tag): add size prop with sm, md, lg options and adjust styles

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 interface TagProps {
   name: string;
+  size?: 'sm' | 'md' | 'lg';
 }
 
-const Tag: React.FC<TagProps> = ({ name }) => (
-  <span className="border border-black rounded-full px-2 pt-1 pb-[.15em] leading-4">{name}</span>
+const sizeClasses = {
+  sm: 'text-xs px-1.5 pt-[.15em] pb-[.05em]',
+  md: 'text-sm px-2 pt-0.75 pb-[.15em]',
+  lg: 'text-base px-2 pt-1 pb-[.15em]',
+};
+
+const Tag: React.FC<TagProps> = ({ name, size = 'md' }) => (
+  <span className={`border border-black rounded-full leading-4 ${sizeClasses[size]}`}>{name}</span>
 );
 
 export default Tag; 
